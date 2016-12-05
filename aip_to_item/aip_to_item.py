d3336e83311ec4e6c5dd7b4fd2c2ef71b2df7faa
Max <eckardm@umich.edu>
2016-12-05 15:01:02 -0500

    gets agent

--- aip_to_item/aip_to_item.py
+++ aip_to_item/aip_to_item.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+from lxml import etree
 
 # `\media\sf_DeepBlue` is auto-mounted
 staging_dir = os.path.join(os.path.sep, "media", "sf_DeepBlue", "deepblue_saf_staging")
@@ -28,3 +29,19 @@ for root, _, files in os.walk(staging_dir):
         os.remove(aip)
 
         aip_dir = os.path.join(temp_dir, os.path.splitext(aip)[0])
+
+        # get agent
+        mets_dir = os.path.join(aip_dir, "data", [name for name in os.listdir(os.path.join(aip_dir, "data")) if name.startswith("METS")][0])
+
+        print mets_dir
+
+        tree = etree.parse(mets_dir)
+        namespaces = {
+            "premis": "info:lc/xmlns/premis-v2",
+            "dc": "http://purl.org/dc/elements/1.1/"
+        }
+
+        user = [agent.xpath("./premis:agentName", namespaces=namespaces)[0].text for agent in tree.xpath("//premis:agent", namespaces=namespaces) if agent.xpath("./premis:agentType", namespaces=namespaces)[0].text == "Archivematica user"][0]
+        username = user.split(", ")[0].split('"')[1].split('"')[0]
+        first_name = user.split(", ")[1].split('"')[1].split('"')[0]
+        last_name = user.split(", ")[2].split('"')[1].split('"')[0]
