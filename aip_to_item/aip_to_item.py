f451644137141678f44d6014e84f8474cd8f3794
Max <eckardm@umich.edu>
2016-12-05 14:42:24 -0500

    unarchives aips

--- aip_to_item/aip_to_item.py
+++ aip_to_item/aip_to_item.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 # `\media\sf_DeepBlue` is auto-mounted
 staging_dir = os.path.join(os.path.sep, "media", "sf_DeepBlue", "deepblue_saf_staging")
@@ -12,3 +13,18 @@ for root, _, files in os.walk(staging_dir):
 
         # make working copy
         shutil.copy(aip, temp_dir)
+
+        aip = os.path.join(temp_dir, name)
+
+        # unarchive aips
+        command = [
+            "unar",
+            "-force-overwrite",
+            "-output-directory", temp_dir,
+            aip
+        ]
+        subprocess.call(command)
+
+        os.remove(aip)
+
+        aip_dir = os.path.join(temp_dir, os.path.splitext(aip)[0])
